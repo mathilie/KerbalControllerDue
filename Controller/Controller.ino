76379487ac4224c83fdaf16e43063996119c544f
Mathias Lien <mathilie@stud.ntnu.no>
2020-10-10 21:46:02 +0200

    Multiple changes, see description
    -Divided digital into toggle and button
    -started setting up loop.
    -made seperate setup method
    -included ArduinoJoystickLiberary

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -4,40 +4,107 @@
 #define FIRSTPIN_RIGHTHEADER 22
 #define LASTPIN_RIGHTHEADER 53
 
-#define LEN_DIGITALPINS LASTPIN_RIGHTHEADER - LASTPIN_RIGHTHEADER
-#define LEN_ANALOGPINS 12
+#define LEN_TOGGLEPINS 16; //half of the bottom pins on the arduino due
+#define LEN_BUTTONPINS 16; //half of the bottom pins on the arduino due
+#define LEN_ANALOGPINS 12;
 
-int digitalPins[LEN_DIGITALPINS];
-bool digitalVals[LEN_DIGITALPINS];
-int analogPins[LEN_ANALOGPINS];
-int analogVals[LEN_ANALOGPINS];
+
+int togglePins[LEN_TOGGLEPINS];
+int toggleVals[LEN_TOGGLEPINS];
+int toggleKeys[LEN_TOGGLEPINS];
+int buttonPins[LEN_BUTTONPINS];
+int buttonKeys[LEN_BUTTONPINS];
+int throttlePin;
+ThreeAxisJoystick stick1, stick2;
+Joystick_ joysticklib;
 
 void checkAnalogInputs()
 {
-  for(int i=0; i<LEN_ANALOGPINS;i++)
+  stick1.readValues();
+  stick2.readValues();
+  joysticklib.setThrottle(digitalRead(throttlePin));
+}
+
+
+void checkToggleInputs()
+{
+  for(int i=0; i<LEN_TOGGLEPINS;i++)
   {
-    analogVals[i]=analogRead(i);
+    int currentval = digitalRead(togglePins[i]);
+    if(currentval!=toggleVals[i]){
+      Keyboard.write(toggleKeys[i]);
+      toggleVals[i]=currentval;
+    }
+  }
+}
+
+void checkButtonInputs()
+{
+  for(int i=0; i<LEN_BUTTONPINS;i++)
+  {
+    if(digitalRead(togglePins[i])==LOW) {
+      Keyboard.write(toggleKeys[i]);
+    }
   }
 }
 
 
-void checkDigitalInputs()
+void setup()
 {
-  for(int i=0; i<LEN_DIGITALPINS;i++)
-  {
-    digitalVals[i] = digitalRead(digitalPins[i]);
+  testSetup();
+}
+
+void loop()
+{
+checkButtonInputs();
+checkToggleInputs();
+checkAnalogInputs();
+}
+
+
+void testSetup(){
+
+  //setting up communication and initial stuff
+  Serial.begin(9600);  // initialize serial communications at 9600 bps
+  Serial.print("Setup");
+  joysticklib.begin();
+
+
+  //---------Setting up digital pins----------
+  int counter = 0;
+  for(int i = FIRSTPIN_RIGHTHEADER; i < LASTPIN_RIGHTHEADER; i++){
+    pinMode(i, INPUT_PULLUP);
+    togglePins[counter] = i;
+    toggleVals[counter] = 0;
+    counter++;
   }
+  counter =0;
+  for(int i = A0; i<A11; i++){
+    analogPins[counter] = i;
+    analogVals[counter] = false;
+    counter++;
+  }
+
+  //------setting up joysticks-------------
+
+
+
+  //----------Setting up throttle----------
+
+
+
+
 }
 
 class Joystick
 {
     public:
       Joystick() = delete;
-      Joystick(int analogPinX, int analogPinY, int digtialPin, int maxAnalogValue, int minAnalogValue): 
-        m_analogPinX(analogPinX), 
-        m_analogPinY(analogPinY), 
-        m_digtialPin(digtialPin), 
-        m_maxAnalogValue(maxAnalogValue), 
+      Joystick(int analogPinX, int analogPinY, int digitalPin, int maxAnalogValue, int minAnalogValue):
+        m_analogPinX(analogPinX),
+        m_analogPinY(analogPinY),
+        m_digitalPin(digitalPin),
+        m_maxAnalogValue(maxAnalogValue),
         m_minAnalogValue(minAnalogValue)
         {}
 
@@ -47,34 +114,36 @@ class Joystick
         int value = map(rawValue, 0, 1023, m_minAnalogValue, m_maxAnalogValue);
         return value;
       }
-      
+
       int getAnalogValueY()
       {
         int rawValue = analogRead(m_analogPinY);
         int value = map(rawValue, 0, 1023, m_minAnalogValue, m_maxAnalogValue);
         return value;
       }
-      
+
+      void readDigitalValue
+
       bool getDigitalValue()
       {
-        bool value = digitalRead(m_digtialPin);
+        bool value = digitalRead(m_digitalPin);
         return value;
       }
-        
+
     protected:
       int m_maxAnalogValue;
       int m_minAnalogValue;
       int m_analogPinX;
       int m_analogPinY;
-      int m_digtialPin;
+      int m_digitalPin;
 };
 
 class ThreeAxisJoystick: public Joystick {
   public:
     ThreeAxisJoystick() = delete;
-    ThreeAxisJoystick(int analogPinX, int analogPinY, int analogPinZ, int digtialPin, int maxAnalogValue, int minAnalogValue): 
-        Joystick(analogPinX, analogPinY, digtialPin, maxAnalogValue, minAnalogValue),
-        m_analogPinZ(analogPinZ) 
+    ThreeAxisJoystick(int analogPinX, int analogPinY, int analogPinZ, int digitalPin, int maxAnalogValue, int minAnalogValue):
+        Joystick(analogPinX, analogPinY, digitalPin, maxAnalogValue, minAnalogValue),
+        m_analogPinZ(analogPinZ)
         {}
 
       int getAnalogValueZ()
@@ -86,50 +155,6 @@ class ThreeAxisJoystick: public Joystick {
 
   private:
     int m_analogPinZ;
-    
+
 };
 
-
-
-/******************************************
-* 
-* Baktanken med koden er at man har én metode for hver inputtype.
-* De digitale sender bare hvis de er endret.
-* Pythonkoden holder styr på om det skal sendes signal videre.
-* Muligens bør trottle skilles ut fra analoge
-*
-*******************************************/
-
-void setup()
-{
-  Serial.begin(9600);  // initialize serial communications at 9600 bps
-  Serial.print("Setup");
-  //Setting up digital pins
-  int counter = 0;
-  for(int i = FIRSTPIN_RIGHTHEADER; i < LASTPIN_RIGHTHEADER; i++){
-    pinMode(i, INPUT_PULLUP);
-    digitalPins[counter] = i;
-    digitalVals[counter] = 0;
-    counter++;
-  }
-  counter =0;
-  for(int i = A0; i<A11; i++){
-    analogPins[counter] = i;
-    analogVals[counter] = false;
-    counter++;
-  }
-   pinMode(LED_BUILTIN, OUTPUT);
-}
-
-void loop()
-{
-  //checkAnalogInputs();
-  //checkDigitalInputs;
-  //digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  //delay(1000);                       // wait for a second
-  //digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1);
-  int value = digitalRead(30);
-  Serial.print("Value of pin 30: ");
-  Serial.println(value);
-}
