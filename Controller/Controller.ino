389c3e848d950054155df10c8a159a1804b78325
Mathias Lien <mathilie@stud.ntnu.no>
2020-10-10 23:58:41 +0200

    Funker sånn ish. se beskrivelse
    Det sendes stuff fra keyboard når knapper trykkes:
    -Toggles funker perfekt.
    -Knapper trenger å få lagt inn et delay før man kan trykke på dem igjen.
    -Joysticken trenger muligens ekstra motstander. potmeteret funket helt i på den ene siden, men funket bra.

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -4,25 +4,26 @@
 #define FIRSTPIN_RIGHTHEADER 22
 #define LASTPIN_RIGHTHEADER 53
 
-#define LEN_TOGGLEPINS 16; //half of the bottom pins on the arduino due
-#define LEN_BUTTONPINS 16; //half of the bottom pins on the arduino due
-#define LEN_ANALOGPINS 12;
+#define LEN_TOGGLEPINS 1 //half of the bottom pins on the arduino due
+#define LEN_BUTTONPINS 2 //half of the bottom pins on the arduino due
+#define LEN_ANALOGPINS 5
 
 
 int togglePins[LEN_TOGGLEPINS];
 int toggleVals[LEN_TOGGLEPINS];
-int toggleKeys[LEN_TOGGLEPINS];
+char toggleKeys[LEN_TOGGLEPINS];
+int analogPins[LEN_ANALOGPINS] = {A6,A7,A8,A9,64};
 int buttonPins[LEN_BUTTONPINS];
-int buttonKeys[LEN_BUTTONPINS];
-int throttlePin;
-ThreeAxisJoystick stick1, stick2;
+char buttonKeys[LEN_BUTTONPINS];
+//ThreeAxisJoystick stick1, stick2;
 Joystick_ joysticklib;
 
 void checkAnalogInputs()
 {
-  stick1.readValues();
-  stick2.readValues();
-  joysticklib.setThrottle(digitalRead(throttlePin));
+  joysticklib.setXAxis(analogRead(analogPins[0]));
+  joysticklib.setYAxis(analogRead(analogPins[1]));
+  joysticklib.setZAxis(analogRead(analogPins[2]));
+  joysticklib.setThrottle(analogRead(analogPins[3]));
 }
 
 
@@ -32,8 +33,10 @@ void checkToggleInputs()
   {
     int currentval = digitalRead(togglePins[i]);
     if(currentval!=toggleVals[i]){
-      Keyboard.write(toggleKeys[i]);
+      Keyboard.print("ajaja");
+      delay(200);
       toggleVals[i]=currentval;
+      delay(1000);
     }
   }
 }
@@ -42,8 +45,9 @@ void checkButtonInputs()
 {
   for(int i=0; i<LEN_BUTTONPINS;i++)
   {
-    if(digitalRead(togglePins[i])==LOW) {
-      Keyboard.write(toggleKeys[i]);
+    if(digitalRead(buttonPins[i])==LOW) {
+      Keyboard.print("hei");
+      delay(10);
     }
   }
 }
@@ -65,37 +69,40 @@ checkAnalogInputs();
 void testSetup(){
 
   //setting up communication and initial stuff
-  Serial.begin(9600);  // initialize serial communications at 9600 bps
-  Serial.print("Setup");
-  joysticklib.begin();
-
+  Keyboard.begin();
+  joysticklib.begin(true);
 
   //---------Setting up digital pins----------
-  int counter = 0;
-  for(int i = FIRSTPIN_RIGHTHEADER; i < LASTPIN_RIGHTHEADER; i++){
-    pinMode(i, INPUT_PULLUP);
-    togglePins[counter] = i;
-    toggleVals[counter] = 0;
-    counter++;
-  }
-  counter =0;
-  for(int i = A0; i<A11; i++){
-    analogPins[counter] = i;
-    analogVals[counter] = false;
-    counter++;
-  }
+  pinMode(22,INPUT_PULLUP);
+  togglePins[0] = 22;
+  toggleVals[0] = LOW;
+  toggleKeys[0] = 't'; //t
+
+  pinMode(26,INPUT_PULLUP);
+  pinMode(30,INPUT_PULLUP);
+  buttonPins[0] = 26;
+  buttonPins[1] = 30;
+  buttonKeys[0] = 'a'; //Space
+  buttonKeys[1] = 'b';//R
+
 
   //------setting up joysticks-------------
 
-
+  joysticklib.setXAxisRange(0,1023);
+  joysticklib.setYAxisRange(0,1023);
+  joysticklib.setZAxisRange(0,1023);
 
   //----------Setting up throttle----------
-
-
-
+  joysticklib.setThrottleRange(0,1023);
 
 }
 
+
+/******************************************
+*                                         *
+*    HELPING CLASSES                      *
+*                                         *
+******************************************/
 class Joystick
 {
     public:
@@ -122,7 +129,6 @@ class Joystick
         return value;
       }
 
-      void readDigitalValue
 
       bool getDigitalValue()
       {
@@ -157,4 +163,3 @@ class ThreeAxisJoystick: public Joystick {
     int m_analogPinZ;
 
 };
-
